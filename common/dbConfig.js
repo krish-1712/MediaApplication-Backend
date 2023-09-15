c26e726af9b12c3e747e0c581a3b49a8656c4717
krish-1712 <krishkannan1712@gmail.com>
2023-09-15 22:25:26 +0530

    latest

--- common/dbConfig.js
+++ common/dbConfig.js
@@ -1,6 +1,7 @@
 
 
-const dbUrl = `mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
+const dbUrl = `mongodb+srv://krishkannan1712:Bala1999@cluster0.v5ee5wa.mongodb.net/YouTube`
+//`mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
 
 module.exports = { dbUrl }     
 
