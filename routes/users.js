a84c621209552242c17ba7a5cc37a7dbdb1e00a1
krish-1712 <krishkannan1712@gmail.com>
2023-09-15 21:28:17 +0530

    loot

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const { userModel } = require('../Schemas/userSchemas');
 const { videoModel } = require('../Schemas/videoSchemas');
 const nodemailer = require('nodemailer')
 const jwt = require('jsonwebtoken')
-
+require('dotenv').config()
 
 // mongoose.connect(dbUrl)
 
