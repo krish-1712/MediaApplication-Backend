600867fda6481d1c9e425c0c26f876c3126411ac
krish-1712 <krishkannan1712@gmail.com>
2023-09-15 19:47:27 +0530

    ride

--- common/dbConfig.js
+++ common/dbConfig.js
@@ -1,11 +1,8 @@
 
-// const mongoose = require('mongoose')
-// const dbUrl = `mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
 
-// module.exports = { dbUrl }     
+const dbUrl = `mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
 
-
-
+module.exports = { dbUrl }     
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,18 @@ const { videoModel } = require('../Schemas/videoSchemas');
 const nodemailer = require('nodemailer')
 const jwt = require('jsonwebtoken')
 
-require('dotenv').config()
+
 // mongoose.connect(dbUrl)
 
 
-
-
-mongoose.connect(`mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`)
-.then(() => console.log('Connected!'));
-
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start your server or perform other tasks here
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 
 
